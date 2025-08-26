26e3fe334e9ceb6c8068c2ff9fb5daa4d9bb1974
roaa.elsaiid <129962651+roaa-hytham@users.noreply.github.com>
2025-08-26 20:27:09 +0300

    Revert "Transaction"

---
+++ TransactionService/src/main/java/com/Virtual_Bank_System/TransactionService/DTO/TransactionCreateDTO.java
@@ -0,0 +1,24 @@
+package com.Virtual_Bank_System.TransactionService.DTO;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TransactionCreateDTO {
+
+	private UUID transactionId;
+	private UUID fromAccountId;
+	private UUID toAccountId;
+	private BigDecimal amount;
+	private String description;
+}
