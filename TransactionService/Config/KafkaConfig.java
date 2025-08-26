cbc554f98b333a79b01710a97db575dee0cd7dea
roaa.elsaiid <129962651+roaa-hytham@users.noreply.github.com>
2025-08-26 20:26:27 +0300

    Add files via upload

---
+++ TransactionService/Config/KafkaConfig.java
@@ -0,0 +1,9 @@
+package com.Virtual_Bank_System.TransactionService.Config;
+
+public class KafkaConfig {
+
+	public KafkaConfig() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ TransactionService/Exception/AccountNotFoundById.java
@@ -0,0 +1,29 @@
+package com.Virtual_Bank_System.TransactionService.Exception;
+
+public class AccountNotFoundById extends RuntimeException {
+
+	public AccountNotFoundById() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public AccountNotFoundById(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AccountNotFoundById(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AccountNotFoundById(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AccountNotFoundById(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
